52eeba2f94da3d95e0f73717074df20e56a1fcbd
deshydtho <48738557+deshydtho@users.noreply.github.com>
2019-04-02 17:12:06 -0500

    Add files via upload

---
+++ DTM/DTM.js
@@ -0,0 +1,55 @@
+$(function() {
+    $('#cars').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#cars-list').toggle(800);
+    });
+    })
+$(function() {
+    $('#cars').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#cars-list1').toggle(800);
+    });
+    })
+$(function() {
+    $('#cars').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#cars-list2').toggle(800);
+    });
+    })
+$(function() {
+    $('#phones').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#phones-list').toggle(800);
+    });
+    })
+$(function() {
+    $('#phones').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#phones-list1').toggle(800);
+    });
+    })
+$(function() {
+    $('#phones').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#phones-list2').toggle(800);
+    });
+    })
+$(function() {
+    $('#houses').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#houses-list').toggle(800);
+    });
+    })
+$(function() {
+    $('#houses').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#houses-list1').toggle(800);
+    });
+    })
+$(function() {
+    $('#houses').on('click', function(event) {
+    console.log('This was clicked!'); 
+    $('#houses-list2').toggle(800);
+    });
+    })
+
